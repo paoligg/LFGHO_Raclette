7931c8e4aa192a1ad63706ff6693a1b26a7ff7e1
jtsd <teyssedrejean@gmail.com>
2024-01-19 15:37:28 +0100

    fix url #

--- src/JoinGame.jsx
+++ src/JoinGame.jsx
@@ -1,6 +1,6 @@
 // JoinGame.jsx
 import React from 'react';
-
+import GetGame from './component/GetGame';
 
 
 function JoinGame() {
@@ -8,8 +8,9 @@ function JoinGame() {
     <div>
       <h1>Rock Paper Scissors Game</h1>
       <button onClick={() => console.log('Joining game...')}>
-        Join a Game
+          Join a Game 
       </button>
+      <GetGame/>
     </div>
   );
 }
